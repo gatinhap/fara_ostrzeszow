c1099c97d7f41aeb612f4d99586576429b1366c7
gatinhap <piorek.julia@gmail.com>
2025-06-21 14:09:50 +0200

    feat-2: setup for nav

---
+++ src/config/StaticTexts.ts
@@ -0,0 +1,15 @@
+export const StaticTexts = {
+  COMPONENTS: {
+    NAVIGATION: {
+      FIRST_LEVEL: {
+        ABOUT: 'o nas',
+        SACRAMENTS: 'sakramenty',
+        OFFICE: 'biuro parafialne',
+        ANNOUNCEMENTS: 'Ogłoszenia',
+        INTENTIONS: 'Intencje',
+        GALLERY: 'Galeria',
+        CONTACT: 'Kontakt',
+      },
+    },
+  },
+};
